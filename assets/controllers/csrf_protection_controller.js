7800eb88ef51c376126a320b2463af2fbecc49e5
Muhamedch00 <mohamed.batman007@gmail.com>
2025-05-22 23:53:31 +0100

    Update csrf_protection_controller.js

--- assets/controllers/csrf_protection_controller.js
+++ assets/controllers/csrf_protection_controller.js
@@ -20,7 +20,7 @@ document.addEventListener('turbo:submit-end', function (event) {
     removeCsrfToken(event.detail.formSubmission.formElement);
 });
 
-export function generateCsrfToken (formElement) {
+export function generateCsrfToken(formElement) {
     const csrfField = formElement.querySelector('input[data-controller="csrf-protection"], input[name="_csrf_token"]');
 
     if (!csrfField) {
@@ -31,8 +31,11 @@ export function generateCsrfToken (formElement) {
     let csrfToken = csrfField.value;
 
     if (!csrfCookie && nameCheck.test(csrfToken)) {
-        csrfField.setAttribute('data-csrf-protection-cookie-value', csrfCookie = csrfToken);
-        csrfField.defaultValue = csrfToken = btoa(String.fromCharCode.apply(null, (window.crypto || window.msCrypto).getRandomValues(new Uint8Array(18))));
+        csrfCookie = csrfToken; // ✅ Correction appliquée ici
+        csrfField.setAttribute('data-csrf-protection-cookie-value', csrfCookie);
+        csrfField.defaultValue = csrfToken = btoa(
+            String.fromCharCode.apply(null, (window.crypto || window.msCrypto).getRandomValues(new Uint8Array(18)))
+        );
         csrfField.dispatchEvent(new Event('change', { bubbles: true }));
     }
 
@@ -42,7 +45,7 @@ export function generateCsrfToken (formElement) {
     }
 }
 
-export function generateCsrfHeaders (formElement) {
+export function generateCsrfHeaders(formElement) {
     const headers = {};
     const csrfField = formElement.querySelector('input[data-controller="csrf-protection"], input[name="_csrf_token"]');
 
@@ -59,7 +62,7 @@ export function generateCsrfHeaders (formElement) {
     return headers;
 }
 
-export function removeCsrfToken (formElement) {
+export function removeCsrfToken(formElement) {
     const csrfField = formElement.querySelector('input[data-controller="csrf-protection"], input[name="_csrf_token"]');
 
     if (!csrfField) {
@@ -70,7 +73,6 @@ export function removeCsrfToken (formElement) {
 
     if (tokenCheck.test(csrfField.value) && nameCheck.test(csrfCookie)) {
         const cookie = csrfCookie + '_' + csrfField.value + '=0; path=/; samesite=strict; max-age=0';
-
         document.cookie = window.location.protocol === 'https:' ? '__Host-' + cookie + '; secure' : cookie;
     }
 }
