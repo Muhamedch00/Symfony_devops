2ae89f96ebab2110eefd612afd9bcc1b29a63ed6
Muhamedch00 <mohamed.batman007@gmail.com>
2025-05-23 11:35:10 +0100

    Update preload.php

--- config/preload.php
+++ config/preload.php
@@ -1,5 +1,7 @@
 <?php
 
-if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
-    require_once dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
+$preloadFile = dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php';
+
+if (is_file($preloadFile)) {
+    include_once $preloadFile;
 }
