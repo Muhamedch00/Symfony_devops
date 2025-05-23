738944c3fb7c6f8de773f8bff1a8b17ba4f60e56
Muhamedch00 <mohamed.batman007@gmail.com>
2025-05-23 11:35:38 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,13 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+$autoloadRuntime = dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+if (is_file($autoloadRuntime)) {
+    include_once $autoloadRuntime;
+} else {
+    throw new RuntimeException('autoload_runtime.php is missing. Run `composer install`.');
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
