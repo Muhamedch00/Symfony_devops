78c137882a8e44e5b599ba169483eec3c4ee088d
Muhamedch00 <mohamed.batman007@gmail.com>
2025-05-23 13:39:18 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
+
 use App\Kernel;
-$autoloadRuntime = dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+$autoloadRuntime = dirname(_DIR_) . '/vendor/autoload_runtime.php';
+
 if (is_file($autoloadRuntime)) {
-    require $autoloadRuntime;
+    require_once $autoloadRuntime;
 } else {
     class MissingAutoloadRuntimeException extends \RuntimeException {}
-    throw new MissingAutoloadRuntimeException('autoload_runtime.php is missing. Run `composer install`.');
+    throw new MissingAutoloadRuntimeException('autoload_runtime.php is missing. Run composer install.');
 }
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
